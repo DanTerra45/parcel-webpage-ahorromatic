45d5776a5d809db83864e3a54c93e495cd2ae49d
FerRodriguezSilva <fernando.ale.fars@gmail.com>
2024-10-20 05:51:55 -0400

    obtener los ingresos de los datos

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -11,4 +11,20 @@ export class StorageService {
     save_data(data) {
         localStorage.setItem(this.storage_key, JSON.stringify(data));
     }
+
+    add_income(income) {
+        const data = this.get_data();
+        data.incomes.push(income);
+        this.save_data(data);
+    }
+
+    add_fee(fee) {
+        const data = this.get_data();
+        data.fees.push(fee);
+        this.save_data(data);
+    }
+
+    get_incomes() {
+        return this.get_data().incomes;
+      }
 }
