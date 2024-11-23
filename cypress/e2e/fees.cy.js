19f34b0a18cdd01875458594c490ba2631a323a7
WalterRochaSevilla <walterrochasevilla@gmail.com>
2024-11-23 14:21:27 -0400

    cypress, arreglando un pequeño cambio de nombre a una variable de gastos

--- cypress/e2e/fees.cy.js
+++ cypress/e2e/fees.cy.js
@@ -4,7 +4,6 @@ describe('Fee Management', () => {
     cy.visit('/');
     cy.get('a[router_link="/fees"]').click();
   });
-
   it('should add a new fee', () => {
     cy.add_fee('Alquiler', '800', '2024-10-23', 'servicios');
     cy.get('.fee-item').should('have.length', 1);
@@ -12,20 +11,17 @@ describe('Fee Management', () => {
     cy.get('.fee-item').should('contain', '800.00 Bs');
     cy.get('.fee-item').should('contain', 'servicios');
   });
-
   it('should delete a fee', () => {
     cy.add_fee('Alquiler', '800', '2024-10-23', 'servicios');
     cy.get('.fee-item button').click();
     cy.get('.fee-item').should('not.exist');
   });
-
   it('should validate fee form', () => {
-    cy.get('#fee-description').type('Test');
-    cy.get('#fee-amount').clear().type('800');
+    cy.get('#fee_description').type('Test');
+    cy.get('#fee_amount').clear().type('800');
     cy.get('#fee-form button').click();
     cy.get('.fee-item').should('not.exist');
   });
-
   it('should persist fees after page reload', () => {
     cy.add_fee('Alquiler', '800', '2024-10-23', 'servicios');
     cy.reload();
